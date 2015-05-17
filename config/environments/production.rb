cd57d28b6924504dfe8d8692f7e9ae8c3b61481d
Stephen Clem <stevebclem@gmail.com>
2015-05-16 21:29:48 -0700

    routes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'peaceful-mountain-3694.herokuapp.com'
+  host = 'https://peaceful-mountain-3694.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
